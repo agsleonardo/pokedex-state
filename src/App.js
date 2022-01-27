b7340d42e71c409789ad6cb95af73c96fc90ebcb
agsleonardo <agsleonardo@hotmail.com>
2022-01-27 17:13:09 -0300

    styling in progress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Pokedex from './Pokedex';
 import axios from 'axios';
 
 class App extends Component {
-
+  
   constructor() {
     super();
     this.state = {
@@ -19,11 +19,11 @@ class App extends Component {
     this.nextPokemon = this.nextPokemon.bind(this);
     this.getNames();
   }
-
+  
   stringToUpperCase (string) {
     return `${string.substring(0,1).toUpperCase()}${string.slice(1)}`
   }
-
+  
   findIds(type) {
     if (!type || type === 'All') return this.state.list.map((poke) => poke.id);
     return this.state.list.reduce((acc, cur) => (
@@ -31,7 +31,7 @@ class App extends Component {
       ), [])
     }
     
-  async getNames () {
+    async getNames () {
       axios('https://pokeapi.co/api/v2/pokemon?limit=100')
       .then(({ data }) => {
         this.setState((state) => ({
@@ -40,75 +40,78 @@ class App extends Component {
         })
         )
         this.getPoke();
-    })
-
-    .catch((err) => console.log(err))
-  }
-
-  getPoke () {
-    this.state.names.forEach((name) => {
-    axios(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then(({ data }) => this.setState((state) => (
-        {...state,
-          onScreen: this.state.selecteds[0],
-          selecteds: [...this.state.selecteds, data.id],
-          list: [...this.state.list,
-                  {
-                    id: data.id,
-                    name: this.stringToUpperCase(data.name),
-                    averageWeight : {value: data.weight/10, measurementUnit: 'kg'},
-                    image: data.sprites.front_default,
-                    type: this.stringToUpperCase(data.types[0].type.name)
-                  }
-                ]
-        }
-      )))
-    .catch((err) => console.log(err))
-    })
-  }
-
-  setPokemons(type) {
-    const set = this.findIds(type);
-    const flag = set.length > 1 ? false : true
-    this.setState({
-      selecteds: set,
-      onScreen: set[0],
-      next: flag,
-      style: flag ? {backgroundColor:'#a9a9a9'} : {backgroundColor: '#fa6400'}
-    })
-  }
-
-  nextPokemon() {
-    this.setState((state) => ({
-      ...state,
-      onScreen: state.selecteds[state.selecteds.indexOf(state.onScreen)+1] || state.selecteds[0]
-    }))
-  }
-  
-  render() {
-    if (!this.state.list.length) {
-      return <p>Carregando...</p>
+      })
+      
+      .catch((err) => console.log(err))
     }
-    const pokeTypes = [...new Set(this.state.list.map((poke) => poke.type))];
-    pokeTypes.unshift('All');
-    return (
-      <div className="App">
-        <h1> Pokedex </h1>
-        <Pokedex pokemons={this.state.list.filter(({id}) => id === this.state.onScreen)} />
-        <h3>
-          { `${this.state.selecteds.indexOf(this.state.onScreen)+1} de ${this.state.selecteds.length}` }
-        </h3>
-        <div className="button-cointainer">
-          {
-            pokeTypes.map((type, idx) => (
-              <Button key={ idx } type={this.stringToUpperCase(type)} onClick={() => this.setPokemons(type)}/>
-            ))
+    
+    getPoke () {
+      this.state.names.forEach((name) => {
+        axios(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(({ data }) => this.setState((state) => (
+          {...state,
+            onScreen: this.state.selecteds[0],
+            selecteds: [...this.state.selecteds, data.id],
+            list: [...this.state.list,
+              {
+                id: data.id,
+                name: this.stringToUpperCase(data.name),
+                averageWeight : {value: data.weight/10, measurementUnit: 'kg'},
+                image: data.sprites.front_default,
+                type: this.stringToUpperCase(data.types[0].type.name)
+              }
+            ]
           }
-        </div>
-        <Button  type="Próximo pokemon" onClick={this.nextPokemon} disabled={this.state.next} style={this.state.style} />
-      </div>
-    );
-  }
-}
-
-export default App;
+          )))
+          .catch((err) => console.log(err))
+        })
+      }
+      
+      setPokemons(type) {
+        const set = this.findIds(type);
+        const flag = set.length > 1 ? false : true
+        this.setState({
+          selecteds: set,
+          onScreen: set[0],
+          next: flag,
+          style: flag ? {backgroundColor:'#a9a9a9'} : {backgroundColor: '#fa6400'}
+        })
+      }
+      
+      nextPokemon() {
+        this.setState((state) => ({
+          ...state,
+          onScreen: state.selecteds[state.selecteds.indexOf(state.onScreen)+1] || state.selecteds[0]
+        }))
+      }
+      
+      render() {
+        if (!this.state.list.length) {
+          return <p>Carregando...</p>
+        }
+        const pokeTypes = [...new Set(this.state.list.map((poke) => poke.type))];
+        pokeTypes.unshift('All');
+        return (
+          <>
+          <div className="App">
+            <h1> Pokedex </h1>
+            <Pokedex pokemons={this.state.list.filter(({id}) => id === this.state.onScreen)} />
+            <h3>
+            { `${this.state.selecteds.indexOf(this.state.onScreen)+1} de ${this.state.selecteds.length}` }
+            </h3>
+            <div className="button-cointainer">
+            {
+              pokeTypes.map((type, idx) => (
+                <Button key={ idx } type={this.stringToUpperCase(type)} onClick={() => this.setPokemons(type)}/>
+                ))
+              }
+              </div>
+              <Button  type="Próximo pokemon" onClick={this.nextPokemon} disabled={this.state.next} style={this.state.style} />
+            </div>
+            </>
+            );
+          }
+        }
+        
+        export default App;
+        
