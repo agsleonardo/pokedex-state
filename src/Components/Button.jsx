afb63b9e4fad4d8555906b2c8ad758d2b0492bff
agsleonardo <agsleonardo@hotmail.com>
2022-01-27 14:25:10 -0300

    button text first letter upper case

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,7 +5,7 @@ export default class Button extends Component {
   render() {
     const { type, onClick, disabled, style } = this.props;
     return (
-      <button onClick={onClick} disabled={disabled} style={style}>{ type }</button>
+      <button onClick={onClick} disabled={disabled} style={style}>{ `${type.substring(0,1).toUpperCase()}${type.slice(1)}` }</button>
     )
   }
 }
